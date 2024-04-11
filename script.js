717281633156d950b838e764c27de38baebae8a3
maple-Rahim <maple.rahim1@gmail.com>
2024-04-11 21:23:33 +0600

    k

---
+++ script.js
@@ -0,0 +1,18 @@
+let slideIndex = 0;
+showSlide(slideIndex);
+
+function prevSlide() {
+    showSlide(slideIndex -= 1);
+}
+
+function nextSlide() {
+    showSlide(slideIndex += 1);
+}
+
+function showSlide(n) {
+    const slides = document.querySelectorAll('.slide');
+    if (n >= slides.length) {slideIndex = 0}
+    if (n < 0) {slideIndex = slides.length - 1}
+    slides.forEach(slide => slide.style.display = "none");
+    slides[slideIndex].style.display = "block";
+}
